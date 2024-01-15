1d0fb550eb54bf6405b8982e041e4f97584a345b
amir <zannedamir4@gmail.com>
2024-01-15 23:09:37 +0100

    final version

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
-# event_manager
-Imagine that a friend of yours runs a non-profit organization around political activism. A number of people have registered for an upcoming event. She has asked for your help in engaging these future attendees. For the first task, she wants you to find the government representatives for each attendee based on their zip code.
+# Event Manager
+
+This is a simple Ruby script for managing event attendees. The script reads attendee information from a CSV file, cleans and processes the data, and generates thank-you letters for each attendee.
+
+## Table of Contents
+
+- [Features](#features)
+- [Prerequisites](#prerequisites)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Configuration](#configuration)
+
+## Features
+
+- Cleans and formats zip codes.
+- Retrieves legislators based on zip codes using the Google Civic Information API.
+- Generates thank-you letters for attendees.
+- Cleans and formats phone numbers.
+- Finds the most common registration day and hour.
+
+## Prerequisites
+
+Before running the script, make sure you have the following installed:
+
+- Ruby
+- Google Civic Information API Key
+
+## Installation
+
+1. Clone the repository:
+
+   ```bash
+   git clone https://github.com/your-username/event-manager.git
+
+   ```
+
+2. Install required gems:
+
+   gem install google-api-client erb
+
+3. Configure your Google Civic Information API key:
+
+Open the script in a text editor.
+fi you wnanna Replace #'YOUR_GOOGLE_API_KEY' with your actual Google Civic Information API key.
+
+## Usage
+
+Ensure your CSV file (event_attendees.csv) is in the project directory.
+
+1. Run the script:
+   ``bash
+   ruby event_manager.rb
+
+   View the generated thank-you letters in the 'output' directory.
+
+## Configuration
+
+To customize the thank-you letter template, edit the form_letter.erb file.
--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -2,10 +2,12 @@ require 'csv'
 require 'google/apis/civicinfo_v2'
 require 'erb'
 
+# Method to clean and format zip codes
 def clean_zipcode(zipcode)
-  zipcode.to_s.rjust(5,"0")[0..4]
+  zipcode.to_s.rjust(5, "0")[0..4]
 end
 
+# Method to fetch legislators based on a zip code using Google Civic Information API
 def legislators_by_zipcode(zip)
   civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
   civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
@@ -21,7 +23,8 @@ def legislators_by_zipcode(zip)
   end
 end
 
-def save_thank_you_letter(id,form_letter)
+# Method to save thank-you letters in HTML format
+def save_thank_you_letter(id, form_letter)
   Dir.mkdir('output') unless Dir.exist?('output')
 
   filename = "output/thanks_#{id}.html"
@@ -31,24 +34,79 @@ def save_thank_you_letter(id,form_letter)
   end
 end
 
+# Main script starts here
 puts 'EventManager initialized.'
 
-contents = CSV.open(
-  'event_attendees.csv',
-  headers: true,
-  header_converters: :symbol
-)
-
-template_letter = File.read('form_letter.erb')
-erb_template = ERB.new template_letter
-
-contents.each do |row|
-  id = row[0]
-  name = row[:first_name]
-  zipcode = clean_zipcode(row[:zipcode])
-  legislators = legislators_by_zipcode(zipcode)
-
-  form_letter = erb_template.result(binding)
-
-  save_thank_you_letter(id,form_letter)
+# Method to read the CSV file and return a CSV object
+def open_csv
+  CSV.open(
+    'event_attendees.csv',
+    headers: true,
+    header_converters: :symbol
+  )
 end
+
+# Method to clean and format phone numbers
+def clean_phone_numbers(phone_number)
+  phone_number.gsub!(/[()\-,. ]/, '')
+  if phone_number.length == 11 && phone_number[0] == '1'
+    phone_number.slice!(0)
+  elsif phone_number.length > 10 || phone_number.length < 10
+    phone_number = nil
+  else
+    phone_number
+  end
+end
+
+# Method to find the most common registration day
+def most_common_reg_day
+  contents = open_csv
+  reg_day_array = []
+  contents.each do |row|
+    reg_date = row[:regdate]
+    reg_day = Time.strptime(reg_date, '%M/%d/%y %k:%M').strftime('%A')
+    reg_day_array.push(reg_day)
+  end
+  most_common_day = reg_day_array.reduce(Hash.new(0)) do |hash, day|
+    hash[day] += 1
+    hash
+  end
+  most_common_day.max_by { |_k, v| v }[0]
+end
+
+# Method to find the most common registration hour
+def most_common_hour
+  contents = open_csv
+  reg_hour_array = []
+  contents.each do |row|
+    reg_date = row[:regdate]
+    reg_hour = Time.strptime(reg_date, '%M/%d/%y %k:%M').strftime('%k')
+    reg_hour_array.push(reg_hour)
+  end
+  most_common_hour = reg_hour_array.reduce(Hash.new(0)) do |hash, hour|
+    hash[hour] += 1
+    hash
+  end
+  most_common_hour.max_by { |_k, v| v }[0]
+end
+
+#template_letter = File.read('form_letter.erb')
+#erb_template = ERB.new template_letter
+#contents=open_csv
+
+#contents.each do |row|
+  #id = row[0]
+  #name = row[:first_name]
+  #zipcode = clean_zipcode(row[:zipcode])
+  #legislators = legislators_by_zipcode(zipcode)
+  #phone_number = clean_phone_numbers(row[:homephone])
+
+  #form_letter = erb_template.result(binding)
+
+  #save_thank_you_letter(id,form_letter)
+#end
+
+
+# Output the most common registration hour and day
+puts "\nThe most common hour of registration is: #{most_common_hour}:00"
+puts "\nThe most common registration day is: #{most_common_reg_day}"
